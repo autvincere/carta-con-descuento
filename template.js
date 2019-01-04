bb772c7c9a87df00dff8be2614c3d0af48917ba7
Eduardo Palma <eduardo.palma@yr.com>
2019-01-04 11:42:15 -0300

    se styla look de cartas

--- template.js
+++ template.js
@@ -1,7 +1,9 @@
 const templateCartas =
      `
+               <h3>Encuentra la carta premiada y soprendete con un nuevo descuento</h3>
+
                <div id="one" class="card">
-               <div class="face face--front">
+               <div class="face face--front rostro-one">
 
                </div>
 
@@ -11,7 +13,7 @@ const templateCartas =
           </div>
 
           <div id="two" class="card">
-               <div class="face face--front">
+               <div class="face face--front rostro-two">
 
                </div>
 
@@ -21,8 +23,8 @@ const templateCartas =
           </div>
 
           <div id="three" class="card">
-               <div class="face face--front">
-
+               <div class="face face--front rostro-three">
+   
                </div>
 
                <div class="face face--back">
@@ -31,8 +33,8 @@ const templateCartas =
           </div>
 
           <div id="four" class="card">
-               <div class="face face--front">
-
+               <div class="face face--front rostro-one">
+  
                </div>
 
                <div class="face face--back">
@@ -41,7 +43,7 @@ const templateCartas =
           </div>
 
           <div id="five" class="card">
-               <div class="face face--front">
+               <div class="face face--front rostro-two">
 
                </div>
 
@@ -51,12 +53,13 @@ const templateCartas =
           </div>
 
           <div id="six" class="card">
-               <div class="face face--front">
-
+               <div class="face face--front rostro-three">
+    
                </div>
 
                <div class="face face--back">
                     <h2 id= "lose">lose</h2>
                </div>
-          </div>`;
+          </div>
+          `;
 
